3fef422dbd328a9ce5aa60740e8a6e433c462323
Noah Sherman <333105@guhsd.net>
2018-10-26 20:27:29 +0000

    Collude until Betrayed, Then Random

--- sherman.py
+++ sherman.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'TeamEdward.'
+strategy_name = 'Collude, then Random'
+strategy_description = 'After being Betrayed, the player will be'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,6 +17,10 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    if 'b' in (their_history):
+        return 'c' or 'b'
+    else:
+        return 'c'
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
