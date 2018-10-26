40744bf1973932f3ebfc1841404a0dca4d243d12
Elijah Ordone <323213@guhsd.net>
2018-10-26 20:42:14 +0000

    adding Elijah code

--- ordone.py
+++ ordone.py
@@ -6,17 +6,13 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Ur Mum' # Only 10 chars displayed.
+strategy_name = 'Collude until Betray'
+strategy_description = 'Collude until their score is above 0, then betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -24,11 +20,16 @@ def move(my_history, their_history, my_score, their_score):
     # The most recent round is my_history[-1] and their_history[-1].
     
     # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    # Decide whether to return 'c' or 'b'.'''
 
-    
+    if len(my_history) == 0:
+        return 'c'
+    elif their_history > 0:
+        return 'b'
+        
+        
+            
+            
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
